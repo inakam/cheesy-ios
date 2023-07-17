357f39333871e44c612adb07db6b9880e68604a5
Ibuki Nakamura <nakamura.ibuki@nifty.co.jp>
2023-07-18 01:20:40 +0900

    現在放送中の番組一覧を入手できるようになった

--- NHKAPIFetcherForSwiftUI/NHKAPIFetcherForSwiftUI/NHKAPIFetcherForSwiftUI/TopListView.swift
+++ NHKAPIFetcherForSwiftUI/NHKAPIFetcherForSwiftUI/NHKAPIFetcherForSwiftUI/TopListView.swift
@@ -8,16 +8,33 @@
 import SwiftUI
 
 struct TopListView: View {
-    let NowOnAirData: NowOnAir = mockNowOnAirData
+    @State private var NowOnAirData: NowOnAir?
+    @State private var isLoading = true
     
     var body: some View {
         NavigationView {
-            List([NowOnAirData.nowonair_list.g1.previous, NowOnAirData.nowonair_list.g1.present, NowOnAirData.nowonair_list.g1.following], id: \.id) { item in
-                NavigationLink(destination: DetailView(programId: item.id))
-                {
-                    RowView(programData: item)
+            if let NowOnAirData = NowOnAirData {
+                List([NowOnAirData.nowonair_list.g1.previous, NowOnAirData.nowonair_list.g1.present, NowOnAirData.nowonair_list.g1.following], id: \.id) { item in
+                    NavigationLink(destination: DetailView(programId: item.id))
+                    {
+                        RowView(programData: item)
+                    }
                 }
-            }.navigationBarTitle(Text("現在放送中の番組一覧"))
+                .navigationBarTitle(Text("現在放送中の番組一覧"))
+            } else if isLoading {
+                Text ("Loading...")
+            } else {
+                Text("Failed to load data.")
+            }
+        }
+        .task{
+            do {
+                isLoading = true
+                NowOnAirData = try await fetchNowOnAirData()
+            } catch {
+                print("Failed to fetch data: \(error)")
+            }
+            isLoading = false
         }
     }
 }
@@ -26,4 +43,14 @@ struct TopListView_Previews: PreviewProvider {
     static var previews: some View {
         TopListView()
     }
+}
+
+func fetchNowOnAirData() async throws -> NowOnAir {
+    let url = URL(string: "https://api.nhk.or.jp/v2/pg/now/130/g1.json?key=\(NHKAPIKEY)")!
+
+    let (data, _) = try await URLSession.shared.data(from: url)
+    let decoder = JSONDecoder()
+    let NowOnAirData = try decoder.decode(NowOnAir.self, from: data)
+    
+    return NowOnAirData
 }
